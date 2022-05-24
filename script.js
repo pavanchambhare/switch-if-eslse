648129625331d3d8e1c55cdb1e8111bb59569144
pavanchambhare <pavanchambhare88@gmail.com>
2022-05-24 21:47:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+
+let anime =  "gohan" ;
+
+switch (anime) {
+    case  'Naruto':
+        console.log("My Fevorite Anime")
+        break;
+        case  'boruto':
+        console.log("Nice Character")
+        break;
+        case  'goku':
+        console.log("Power of God")
+        break;
+        case  'gohan':
+        console.log("Goku Son")
+        break;
+        case  'kakashi':
+        console.log("Power of Sharingan")
+        break;
+
+    default:
+        console.log("I Don't No What Is Anime")
+  
+        
+}
+
+console.log(anime)
+
+let i =2
+let x =5
+
+if( i > x){
+   console.log("i is greater than")
+}
+else{
+    console.log("x si greater than")
+}
